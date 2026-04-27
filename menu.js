5ba42666b2ae3d7b0a2d514202ee5548b6f4412d
Nadjen Martíns Álvarez <nadjenma@aulaestudio.es>
2026-04-27 20:18:37 +0200

    readline

---
+++ menu.js
@@ -0,0 +1,13 @@
+const readline = require("readline")
+const luxon = require("luxon")
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+async function menu() {
+    rl.on(
+        
+    )
+}
